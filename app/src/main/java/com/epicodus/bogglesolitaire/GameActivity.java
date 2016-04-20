e81af4a57d0f2ef6b5b34869b0d91e99d830a1e1
Daren Schaad and Kyle Derrick <darenschaad@gmail.com>
2016-04-20 14:26:27 -0700

    add words to an array and styled display

--- app/src/main/java/com/epicodus/bogglesolitaire/GameActivity.java
+++ app/src/main/java/com/epicodus/bogglesolitaire/GameActivity.java
@@ -3,12 +3,19 @@ package com.epicodus.bogglesolitaire;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class GameActivity extends AppCompatActivity {
+    public static final String TAG = GameActivity.class.getSimpleName();
 
     @Bind(R.id.letter1) TextView mLetter1;
     @Bind(R.id.letter2) TextView mLetter2;
@@ -19,6 +26,10 @@ public class GameActivity extends AppCompatActivity {
     @Bind(R.id.letter7) TextView mLetter7;
     @Bind(R.id.letter8) TextView mLetter8;
     @Bind(R.id.letter9) TextView mLetter9;
+    @Bind(R.id.tvWordView) TextView mTVWordView;
+    @Bind(R.id.submitButton) Button mSubmitButton;
+    @Bind(R.id.getScoreButton) Button mGetScoreButton;
+    List<String> enteredWordArray = new ArrayList<String>();
 
 //    @Bind(R.id.letter4) TextView mLetter4;
     @Override
@@ -28,15 +39,15 @@ public class GameActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         Intent intent = getIntent();
-        String consonant1 = intent.getStringExtra("consonant1");
-        String consonant2 = intent.getStringExtra("consonant2");
-        String consonant3 = intent.getStringExtra("consonant3");
-        String consonant4 = intent.getStringExtra("consonant4");
-        String consonant5 = intent.getStringExtra("consonant5");
-        String consonant6 = intent.getStringExtra("consonant6");
-        String vowel1 = intent.getStringExtra("vowel1");
-        String vowel2 = intent.getStringExtra("vowel2");
-        String vowel3 = intent.getStringExtra("vowel3");
+        final String consonant1 = intent.getStringExtra("consonant1");
+        final String consonant2 = intent.getStringExtra("consonant2");
+        final String consonant3 = intent.getStringExtra("consonant3");
+        final String consonant4 = intent.getStringExtra("consonant4");
+        final String consonant5 = intent.getStringExtra("consonant5");
+        final String consonant6 = intent.getStringExtra("consonant6");
+        final String vowel1 = intent.getStringExtra("vowel1");
+        final String vowel2 = intent.getStringExtra("vowel2");
+        final String vowel3 = intent.getStringExtra("vowel3");
 
         mLetter1.setText(consonant1);
         mLetter2.setText(vowel1);
@@ -48,7 +59,78 @@ public class GameActivity extends AppCompatActivity {
         mLetter9.setText(vowel3);
         mLetter8.setText(consonant6);
 
+        mLetter1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTVWordView.append(consonant1);
+            }
+        });
+        mLetter2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTVWordView.append(vowel1);
+            }
+        });
+        mLetter3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTVWordView.append(consonant2);
+            }
+        });
+        mLetter4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTVWordView.append(consonant3);
+            }
+        });
+        mLetter5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTVWordView.append(consonant4);
+            }
+        });
+        mLetter6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTVWordView.append(vowel2);
+            }
+        });
+        mLetter7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTVWordView.append(consonant5);
+            }
+        });
+        mLetter8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTVWordView.append(consonant6);
+            }
+        });
+        mLetter9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTVWordView.append(vowel3);
+            }
+        });
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String enteredWord = mTVWordView.getText().toString();
+                if (enteredWord.length() > 2) {
+                    enteredWordArray.add(enteredWord);
+                }mTVWordView.setText("");
+                for (String i : enteredWordArray){
+                    Log.d(TAG, i);
+                }
+            }
+        });
+        mGetScoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
 
     }
 }
